d522f5931cb86378bcc83cea1e06d24b8df4e8b7
capython59 <62896832+capython59@users.noreply.github.com>
2020-09-25 22:24:39 +0300

    Добавил реализацию hex -> bin в виде башни по 1 байту.

--- lab1.cpp
+++ lab1.cpp
@@ -3,162 +3,270 @@
 #include <string>
 #include <fstream>
 #include <cmath>
+#include <string>
+#include <sstream>
 
-
+using namespace std;
 
 /*
-        How it works
-      ______first 2 bit show what type of packet is it ( 11 - header 
-      | |                                                10 - packets in the middle of sending message 
-      | |                                                00 - end of message)
-      | |
-      | | ___________________ The other 6 bit in header packet show how many packets will be send (There are will be sended 8 packets with 16 bit payload  110 111 101 101 101 000)
-    _/__//___________/
-    |1|1|0|0|0|1|0|0|
+		How it works
+	  ______first 2 bit show what type of packet is it ( 11 - header
+	  | |                                                10 - packets in the middle of sending message
+	  | |                                                00 - end of message)
+	  | |
+	  | | ___________________ The other 6 bit in header packet show how many packets will be send (There are will be sended 8 packets with 16 bit payload  110 111 101 101 101 000)
+	_/__//___________/
+	|1|1|0|0|0|1|0|0|
 
-          _____________payload
-          |    |
-          |    |__________ xor with payload with payload from other packer (but here will be xor with 000)
-    _____/____//____/
-     0 1 2 3 4 5 6 7
-    |1|0|1|1|0|1|1|0|
-    |1|0|1|1|1|0|0|1| 
-    |1|0|1|0|1|0|1|0|
-    |1|0|1|0|1|0|0|0|
-    |1|0|1|0|1|0|0|0|
-    |0|0|0|0|0|1|0|1| - last packet (coz 00)
-    -----------------
+		  _____________payload
+		  |    |
+		  |    |__________ xor with payload with payload from other packer (but here will be xor with 000)
+	_____/____//____/
+	 0 1 2 3 4 5 6 7
+	|1|0|1|1|0|1|1|0|
+	|1|0|1|1|1|0|0|1|
+	|1|0|1|0|1|0|1|0|
+	|1|0|1|0|1|0|0|0|
+	|1|0|1|0|1|0|0|0|
+	|0|0|0|0|0|1|0|1| - last packet (coz 00)
+	-----------------
 
 */
 
 std::string BinStr = "";
 
+
+
 int convertBinaryToDecimal(long long n)
 {
-    int decimalNumber = 0, i = 0, remainder;
-    while (n!=0){
-        remainder = n%10;
-        n /= 10;
-        decimalNumber += remainder*pow(2,i);
-        ++i;
-    }
-    return decimalNumber;
+	int decimalNumber = 0, i = 0, remainder;
+	while (n != 0) {
+		remainder = n % 10;
+		n /= 10;
+		decimalNumber += remainder * pow(2, i);
+		++i;
+	}
+	return decimalNumber;
 }
 
-void outputHex(int num,std::ofstream &output)
+void outputHex(int num, std::ofstream &output)
 {
-    if(num < 10){
-        output<< num;
-    }
-    else{
-         switch (num){
-         case 10:
-             output<< 'A';
-             break;
-         case 11:
-             output<< 'B';
-             break;
-         case 12:
-             output<< 'C';
-             break;
-         case 13:
-             output<< 'D';
-             break;
-         case 14:
-             output<< 'E';
-             break;
-         case 15:
-             output<< 'F';
-             break;
-        
-         default:
-             break;
-         }
-    }
-    
+	if (num < 10) {
+		output << num;
+	}
+	else {
+		switch (num) {
+		case 10:
+			output << 'A';
+			break;
+		case 11:
+			output << 'B';
+			break;
+		case 12:
+			output << 'C';
+			break;
+		case 13:
+			output << 'D';
+			break;
+		case 14:
+			output << 'E';
+			break;
+		case 15:
+			output << 'F';
+			break;
+
+		default:
+			break;
+		}
+	}
+
 }
 
-void print_bin_to_hex( std::ofstream &output)
+void print_bin_to_hex(std::ofstream &output)
 {
-    int count = BinStr.size();
-    count = count - (count % 4);
-    //std::string sliceBinStr = BinStr.substr(0,count);
-    for (int i = 0; i < count; i+=4){
-        outputHex(convertBinaryToDecimal(stoi(BinStr.substr(i,4))),output); //uffffffff some cake
-    }
-}
-
-
-bool pckt_check( std::string pckt_buf, std::string pckt )
-{
-            if ( (pckt[5] - '0') !=  (pckt_buf[2] - '0' ) ^ (pckt[2] - '0')   )
-                return false;
-            if ( (pckt[6] - '0') !=  (pckt_buf[3] - '0' ) ^ (pckt[3] - '0')   )
-                return false;
-            if ( (pckt[7] - '0') !=  (pckt_buf[4] - '0' ) ^ (pckt[4] - '0')   )
-                return false;
-    return true;
+	int count = BinStr.size();
+	count = count - (count % 4);
+	//std::string sliceBinStr = BinStr.substr(0,count);
+	for (int i = 0; i < count; i += 4) {
+		outputHex(convertBinaryToDecimal(stoi(BinStr.substr(i, 4))), output); //uffffffff some cake
+	}
 }
 
 
-int read_packets(int num_packet, std::ifstream &input,std::string pckt_buf)
+bool pckt_check(std::string pckt_buf, std::string pckt)
 {
-    int count = num_packet;
-    std::string pckt;
-    std::string payload;
-    
+	if ((pckt[5] - '0') != (pckt_buf[2] - '0') ^ (pckt[2] - '0'))
+		return false;
+	if ((pckt[6] - '0') != (pckt_buf[3] - '0') ^ (pckt[3] - '0'))
+		return false;
+	if ((pckt[7] - '0') != (pckt_buf[4] - '0') ^ (pckt[4] - '0'))
+		return false;
+	return true;
+}
 
-    while (count){
 
-        input >> pckt;
-        
-        if (input.eof())
-           return -1;
+int read_packets(int num_packet, std::ifstream &input, std::string pckt_buf)
+{
+	int count = num_packet;
+	std::string pckt;
+	std::string payload;
 
-        if ( ( count != 1 ) && ( pckt[0] == '1' ) && ( pckt[1] == '0' )){
-            if(!pckt_check(pckt_buf,pckt))
-                return -2;
-            BinStr = BinStr + pckt.substr(2,3);
-            pckt_buf = pckt;
-            count--;
-            continue;
-        }
 
-        if ( ( count == 1 ) && (pckt[0] == '0' ) && (pckt[1] == '0' ) ){
-            if(!pckt_check(pckt_buf,pckt))
-                return -2;
-            BinStr = BinStr + pckt.substr(2,3);
-            return 0;
-        }
+	while (count) {
 
-        return -2;
-    }
+		input >> pckt;
 
-    return -1;
-    
+		if (input.eof())
+			return -1;
+
+		if ((count != 1) && (pckt[0] == '1') && (pckt[1] == '0')) {
+			if (!pckt_check(pckt_buf, pckt))
+				return -2;
+			BinStr = BinStr + pckt.substr(2, 3);
+			pckt_buf = pckt;
+			count--;
+			continue;
+		}
+
+		if ((count == 1) && (pckt[0] == '0') && (pckt[1] == '0')) {
+			if (!pckt_check(pckt_buf, pckt))
+				return -2;
+			BinStr = BinStr + pckt.substr(2, 3);
+			return 0;
+		}
+
+		return -2;
+	}
+
+	return -1;
+
+}
+
+
+void HexToBinTower(string hexdec, std::ofstream &output) //hexdec - исходное hex число из input.txt. ¬ыводит в output.txt по 1 байту в столбик.
+{
+	long int i = 0;
+	string temp; //временна€ строка, в которую переводитс€ исходное hex в bin в строчку.
+
+	while (hexdec[i]) {
+
+		switch (hexdec[i]) {
+		case '0':
+			temp += "0000";
+			break;
+		case '1':
+			temp += "0001";
+			break;
+		case '2':
+			temp += "0010";
+			break;
+		case '3':
+			temp += "0011";
+			break;
+		case '4':
+			temp += "0100";
+			break;
+		case '5':
+			temp += "0101";
+			break;
+		case '6':
+			temp += "0110";
+			break;
+		case '7':
+			temp += "0111";
+			break;
+		case '8':
+			temp += "1000";
+			break;
+		case '9':
+			temp += "1001";
+			break;
+		case 'A':
+		case 'a':
+			temp += "1010";
+			break;
+		case 'B':
+		case 'b':
+			temp += "1011";
+			break;
+		case 'C':
+		case 'c':
+			temp += "1100";
+			break;
+		case 'D':
+		case 'd':
+			temp += "1101";
+			break;
+		case 'E':
+		case 'e':
+			temp += "1110";
+			break;
+		case 'F':
+		case 'f':
+			temp += "1111";
+			break;
+		default:
+			cout << "\nInvalid hexadecimal digit " << hexdec[i];
+		}
+		i++;
+	}
+	//Ѕлок вывода в output.txt длинного bin числа в столбик по 1 байту.
+	int noBlock = temp.length() / 8; //проверка числа байт.
+	int len = 0;
+	for (int i = 0; i < noBlock; i++)
+	{
+		output << temp.substr(len, 8) << std::endl; //последовательный вывод подстрок из основной строки, которые начинаютс€ на каждом восьмом(считаем от нул€) бите и длинной 8 бит.
+		len += 8;
+	}
+
 }
 
 int main()
 {
- 
-    int num_packet = 0;
-    int operation;
-    std::string msg, msg_buf;
-    std::ifstream input  ("input.txt");
-    std::ofstream output ("output.txt");
+	/*
+	—6 -> 11000110
+	B6 -> 10110110
+	B9 -> 10111001
+	AA -> 10101010
+	A8 -> 10101000
+	A8 -> 10101000
+    05 -> 00000101
 
-    input >> msg;
-    num_packet =  stoi(msg.substr(2,6));
-    num_packet = convertBinaryToDecimal(num_packet);
+	main функцию вз€л свою, сам решай куда это впихнуть :)
+	*/
+	std::ifstream input("input.txt");
+	std::ofstream output("output.txt");
+	
+	string number;
+	input >> number; //hex число из input.txt в строку дл€ дальнейшей работы.
 
-    if (msg[0] == '1' && msg[1] == '1')
-        read_packets(num_packet,input,msg);
+	HexToBinTower(number, output); 
 
-    print_bin_to_hex(output);
-
-    return 0;
-
+	system("pause");
+	return 0;
+}
 
 
+/*
+int main()
+{
+
+	int num_packet = 0;
+	int operation;
+	std::string msg, msg_buf;
+	std::ifstream input("input.txt");
+	std::ofstream output("output.txt");
+
+	input >> msg;
+	num_packet = stoi(msg.substr(2, 6));
+	num_packet = convertBinaryToDecimal(num_packet);
+
+	if (msg[0] == '1' && msg[1] == '1')
+		read_packets(num_packet, input, msg);
+
+	print_bin_to_hex(output);
+
+	return 0;
 
 }
+*/
